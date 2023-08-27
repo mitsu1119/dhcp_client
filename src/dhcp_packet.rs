97483178230b078dbaf928e81a902e5829dae812
mitsu1119 <hachimitsu1119@gmail.com>
2023-08-27 09:55:04 +0900

    dhcp のパケットの構造

--- src/dhcp_packet.rs
+++ src/dhcp_packet.rs
@@ -1,82 +1,27 @@
-#[path = "./octets.rs"]
-pub mod octets;
-use octets::Octets;
-
-pub const XID_LEN:    usize = 4;
-pub const SECS_LEN:   usize = 2;
-pub const FLAGS_LEN:  usize = 2;
-pub const CIADDR_LEN: usize = 4;
-pub const YIADDR_LEN: usize = 4;
-pub const SIADDR_LEN: usize = 4;
-pub const GIADDR_LEN: usize = 4;
-pub const CHADDR_LEN: usize = 16;
-pub const SNAME_LEN:  usize = 64;
-pub const FILE_LEN:   usize = 128;
-pub const MAGICCOOKIE_LEN: usize = 4;
-pub const ALL_LEN:    usize = 4 + XID_LEN + SECS_LEN + FLAGS_LEN + CIADDR_LEN + YIADDR_LEN + SIADDR_LEN + GIADDR_LEN + CHADDR_LEN + SNAME_LEN + FILE_LEN + MAGICCOOKIE_LEN;
-
 // TODO: options は一旦マジッククッキーだけ
 #[derive(Debug)]
-pub struct DhcpPacket {
+pub struct MutableDhcpPacket {
     op:     Op,
     htype:  Htype,
     hlen:   u8,
     hops:   u8,
-    xid:    Octets<XID_LEN>,
-    secs:   Octets<SECS_LEN>,
-    flags:  Octets<FLAGS_LEN>,
-    ciaddr: Octets<CIADDR_LEN>,
-    yiaddr: Octets<YIADDR_LEN>,
-    siaddr: Octets<SIADDR_LEN>,
-    giaddr: Octets<GIADDR_LEN>,
-    chaddr: Octets<CHADDR_LEN>,
-    sname:  Octets<SNAME_LEN>,
-    file:   Octets<FILE_LEN>,
-    options:Octets<MAGICCOOKIE_LEN>
+    xid:    u32,
+    secs:   u16,
+    flags:  u16,
+    ciaddr: u32,
+    yiaddr: u32,
+    siaddr: u32,
+    giaddr: u32,
+    chaddr: u128,
+    sname:  [u8; 64],
+    file:   [u8; 128],
+    options:u8
 }
 
-impl DhcpPacket {
-    pub fn new() -> Self {
-        Self {
-            op:     Op::BOOTREQUEST,
-            htype:  Htype::Ethernet,
-            hlen:   0,
-            hops:   0,
-            xid:    Octets::new(),
-            secs:   Octets::new(),
-            flags:  Flags::Unicast,
-            ciaddr: Octets::new(),
-            yiaddr: Octets::new(),
-            siaddr: Octets::new(),
-            giaddr: Octets::new(),
-            chaddr: Octets::new(),
-            sname:  Octets::new(),
-            file:   Octets::new(),
-            options: MAGICCOOKIE
-        }
+impl MutableDhcpPacket {
+    fn minimum_packet_size() -> usize {
+        237
     }
-
-    pub fn get_bytes(&self) -> [u8; ALL_LEN] {
-        let res = [self.op.get_bytes(), self.htype.get_bytes(), &[self.hlen], &[self.hops], self.xid.get_bytes(), self.secs.get_bytes(), self.flags.get_bytes(), self.ciaddr.get_bytes(), self.yiaddr.get_bytes(), self.siaddr.get_bytes(), self.giaddr.get_bytes(), self.chaddr.get_bytes(), self.sname.get_bytes(), self.file.get_bytes(), self.options.get_bytes()].concat().try_into().expect("error");
-        res
-    }
-
-    pub fn set_op(&mut self, op: Op) { self.op = op; }
-    pub fn set_htype(&mut self, htype: Htype) { self.htype = htype; }
-    pub fn calc_and_set_hlen(&mut self) { 
-        self.hlen = match self.htype {
-            Htype::Ethernet => 6,
-        }
-    }
-    pub fn set_hops(&mut self, hops: u8) { self.hops = hops; }
-    pub fn set_xid(&mut self, xid: Octets<XID_LEN>) { self.xid = xid; }
-    pub fn set_secs(&mut self, secs: Octets<SECS_LEN>) { self.secs = secs; }
-    pub fn set_flags(&mut self, flags: Octets<FLAGS_LEN>) { self.flags = flags; }
-    pub fn set_ciaddr(&mut self, ciaddr: Octets<CIADDR_LEN>) { self.ciaddr = ciaddr; }
-    pub fn set_yiaddr(&mut self, yiaddr: Octets<CIADDR_LEN>) { self.yiaddr = yiaddr; }
-    pub fn set_siaddr(&mut self, siaddr: Octets<CIADDR_LEN>) { self.siaddr = siaddr; }
-    pub fn set_giaddr(&mut self, giaddr: Octets<CIADDR_LEN>) { self.giaddr = giaddr; }
-    pub fn set_chaddr(&mut self, chaddr: Octets<CHADDR_LEN>) { self.chaddr = chaddr; }
 }
 
 #[repr(u8)]
@@ -86,35 +31,8 @@ pub enum Op {
     BOOTREPLY   = 2
 }
 
-impl Op {
-    fn get_bytes(&self) -> &[u8] {
-        match self {
-            Op::BOOTREQUEST => &[1u8],
-            Op::BOOTREPLY => &[2u8],
-        }
-    }
-}
-
 #[repr(u8)]
 #[derive(Debug)]
-pub enum Htype {
+pub enum HType {
     Ethernet = 1
-}
-
-impl Htype {
-    fn get_bytes(&self) -> &[u8] {
-        match self {
-            Htype::Ethernet => &[1u8]
-        }
-    }
-}
-
-pub const MAGICCOOKIE: Octets<MAGICCOOKIE_LEN> = Octets::<MAGICCOOKIE_LEN> { data: [0x63, 0x82, 0x53, 0x63] };
-
-#[derive(Debug)]
-pub struct Flags {}
-
-impl Flags {
-    pub const Unicast: Octets<FLAGS_LEN> = Octets::<FLAGS_LEN> { data: [0u8; FLAGS_LEN] };
-    pub const Broadcast: Octets<FLAGS_LEN> = Octets::<FLAGS_LEN> { data: [0b10000000, 0] };
 }
