5c8e557ae7e3daa4084fe82dbe62c15f03e4513e
mitsu1119 <hachimitsu1119@gmail.com>
2023-08-27 09:20:27 +0900

    ブロードキャストのipヘッダを作成

--- src/broadcast.rs
+++ src/broadcast.rs
@@ -6,10 +6,36 @@ use pnet::packet::ethernet::{MutableEthernetPacket, EtherTypes};
 use pnet::packet::Packet;
 use pnet::util::MacAddr;
 
-pub fn send_broadcast_l3(src_port: u16, dest_port: u16, interface: &NetworkInterface, payload: &Vec<u8>) {
-    send_broadcast_l2(interface, payload);
+use pnet::packet::ip::IpNextHeaderProtocols;
+use pnet::packet::ipv4;
+use pnet::packet::ipv4::MutableIpv4Packet;
+use std::net::Ipv4Addr;
+
+/* レイヤ 3 で payload の内容のブロードキャストを送る */
+/* IPv4 のみ対応 */
+pub fn send_broadcast(src_port: u16, dest_port: u16, interface: &NetworkInterface, payload: &Vec<u8>) {
+    let mut ipv4_buffer: Vec<u8> = vec![0; ipv4::MutableIpv4Packet::minimum_packet_size() + payload.len()];
+    let mut ipv4_packet = MutableIpv4Packet::new(&mut ipv4_buffer).unwrap();
+
+    ipv4_packet.set_version(4);
+    ipv4_packet.set_header_length((MutableIpv4Packet::minimum_packet_size() / 4).try_into().unwrap());
+    ipv4_packet.set_ecn(0);
+    ipv4_packet.set_dscp(4);
+    ipv4_packet.set_total_length(ipv4_packet.packet().len() as u16);
+    ipv4_packet.set_identification(0);
+    ipv4_packet.set_flags(0);
+    ipv4_packet.set_fragment_offset(0);
+    ipv4_packet.set_ttl(128);
+    ipv4_packet.set_next_level_protocol(IpNextHeaderProtocols::Udp);
+    ipv4_packet.set_source(Ipv4Addr::new(0, 0, 0, 0));
+    ipv4_packet.set_destination(Ipv4Addr::new(255, 255, 255, 255));
+    ipv4_packet.set_checksum(ipv4::checksum(&ipv4_packet.to_immutable()));
+    ipv4_packet.set_payload(payload);
+
+    send_broadcast_l2(interface, &ipv4_packet.packet().to_vec());
 }
 
+/* レイヤ 2 で payload の内容のブロードキャストを送る */
 pub fn send_broadcast_l2(interface: &NetworkInterface, payload: &Vec<u8>) {
     let (mut tx, mut rx) = match datalink::channel(&interface, Default::default()) {
         Ok(Ethernet(tx, rx)) => (tx, rx),
@@ -24,6 +50,7 @@ pub fn send_broadcast_l2(interface: &NetworkInterface, payload: &Vec<u8>) {
     ethernet_packet.set_destination(MacAddr::broadcast());
     ethernet_packet.set_source(interface.mac.unwrap());
     ethernet_packet.set_ethertype(EtherTypes::Ipv4);
+    ethernet_packet.set_payload(payload);
 
     tx.send_to(ethernet_packet.packet(), None).expect("wow");
 }
